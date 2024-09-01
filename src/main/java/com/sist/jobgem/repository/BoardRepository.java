699538a53fa0158bb351785952db3dddebd3118c
DESKTOP-P15R8BC\JY <wjddl5796@naver.com>
2024-09-01 15:11:29 +0900

    Feat: JPA Pageable 추가

--- src/main/java/com/sist/jobgem/repository/BoardRepository.java
+++ src/main/java/com/sist/jobgem/repository/BoardRepository.java
@@ -1,5 +1,9 @@
 package com.sist.jobgem.repository;
 
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import org.springframework.stereotype.Repository;
@@ -8,5 +12,9 @@ import com.sist.jobgem.entity.Board;
 
 @Repository
 public interface BoardRepository extends JpaRepository<Board, Integer> {
+
+  // List<Board> findByBoTypeAndBoStatus(int boType, int boStatus, Pageable
+  // pageable);
+  Page<Board> findByBoTypeAndBoStatus(int boType, int boStatus, Pageable pageable);
 
 }
--- src/main/java/com/sist/jobgem/service/BoardService.java
+++ src/main/java/com/sist/jobgem/service/BoardService.java
@@ -1,10 +1,14 @@
 package com.sist.jobgem.service;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import com.sist.jobgem.dto.BoardDto;
@@ -18,23 +22,38 @@ public class BoardService {
   @Autowired
   private BoardRepository boardRepository;
 
-  public List<BoardDto> getAll() {
-    List<Board> list = boardRepository.findAll(); // status + bo_type 조건으로 변경
-    BoardDto[] ar = null;
-
-    List<BoardDto> dto_list = BoardMapper.INSTANCE.toDtoList(list);
-
-    // if (!dto_list.isEmpty()) {
-    // ar = new BoardDto[dto_list.size()];
-    // dto_list.toArray(ar);
-    // }
-    return dto_list;
-  }
+  // public List<BoardDto> getBbsList(int boType, int boStatus, Pageable pageable)
+  // {
+  // List<Board> list = boardRepository.findByBoTypeAndBoStatus(boType, boStatus,
+  // pageable);
+  // List<BoardDto> dto_list = BoardMapper.INSTANCE.toDtoList(list);
+  // return dto_list;
+  // }
 
   public BoardDto getView(int id) {
     Board board = boardRepository.findById(id).get();
     BoardDto boardDto = BoardMapper.INSTANCE.toDto(board);
     return boardDto;
+  }
+
+  public Page<BoardDto> getBbsList(int boType, int boStatus, Pageable pageable) {
+    Pageable pageable2 = PageRequest.of(pageable.getPageNumber(), pageable.getPageSize(),
+        Sort.by(Sort.Direction.DESC, "id"));
+
+    Page<Board> boardPage = boardRepository.findByBoTypeAndBoStatus(boType, boStatus, pageable2);
+
+    // Board -> BoardDto 변환
+    List<BoardDto> boardDtoList = boardPage.getContent().stream()
+        .map(this::convertToDto)
+        .collect(Collectors.toList());
+
+    // 변환된 DtoList를 사용하여 새로운 Page<BoardDto> 객체를 생성
+    return new PageImpl<>(boardDtoList, pageable2, boardPage.getTotalElements());
+  }
+
+  // Board 엔티티를 BoardDto로 변환하는 메소드
+  private BoardDto convertToDto(Board board) {
+    return BoardDto.fromEntity(board);
   }
 
 }
