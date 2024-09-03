975740d260d3cf6443f44fb22d0d6de50c7e64bc
KYBYK <dbqks56231106@gmail.com>
2024-09-03 12:18:03 +0900

    Fix : User entity와 이름이 다른 변수명 수정

--- src/main/java/com/sist/jobgem/dto/UserDto.java
+++ src/main/java/com/sist/jobgem/dto/UserDto.java
@@ -3,10 +3,6 @@ package com.sist.jobgem.dto;
 import java.time.Instant;
 import java.time.LocalDate;
 
-import com.sist.jobgem.entity.Board;
-import com.sist.jobgem.entity.Jobseeker;
-import com.sist.jobgem.entity.User;
-
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -20,8 +16,8 @@ public class UserDto {
   private Integer id;
   private String usId;
   private String usPw;
-  private Instant joinDate;
-  private LocalDate leaveDate;
+  private Instant usJoinDate;
+  private LocalDate usLeaveDate;
   private Integer usType;
   private Integer usState;
   private String accessToken;
