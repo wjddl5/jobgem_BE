28489a4a6b0ff4c18184a63eb50d6fd334ddbfa3
Kim YuBin <dbqks56231106@gmail.com>
2024-09-03 18:16:09 +0900

    Fix : @Data 대신 어노테이션 한 개씩 작성

--- src/main/java/com/sist/jobgem/dto/UserDto.java
+++ src/main/java/com/sist/jobgem/dto/UserDto.java
@@ -5,10 +5,12 @@ import java.time.LocalDate;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-@Data
+@Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
