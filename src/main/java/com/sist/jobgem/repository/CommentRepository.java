529594ab43d786a34e4c2068e2314561759cc959
JeongYi <165870505+wjddl5@users.noreply.github.com>
2024-09-03 17:25:23 +0900

    Feat: 게시글 삭제, 댓글 삭제 , 댓글 작성 기능 및 페이지 변수 수정 (#24)
    * Feat: JPA Pageable 추가
    
    * 작업 내용 설명
    
    * Describe your changes here
    
    * Fix: Dto 오류 임시 수정
    
    * Fix: 임시오류수정 2
    
    * Fix : Dto, Db설정 충돌 수정
    
    * Fix : requestParam 어노테이션 추가
    
    * Feat: 공지사항 삭제, 댓글삭제 기능 추가
    
    * Feat: 댓글 작성 기능 추가

--- src/main/java/com/sist/jobgem/repository/CommentRepository.java
+++ src/main/java/com/sist/jobgem/repository/CommentRepository.java
@@ -1,10 +1,21 @@
 package com.sist.jobgem.repository;
 
 import com.sist.jobgem.entity.Comment;
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Integer> {
-    
+  List<Comment> findByBoIdxAndCommStatus(Integer boIdx, Integer commStatus);
+
+  @Modifying
+  @Query("UPDATE Comment c SET c.commStatus = 0 WHERE c.id = :id")
+  int updateCommentStatus(@Param("id") int id);
+
 }
---
+++ src/main/java/com/sist/jobgem/service/CommentService.java
@@ -0,0 +1,57 @@
+package com.sist.jobgem.service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.sist.jobgem.dto.CommentDto;
+import com.sist.jobgem.entity.Comment;
+import com.sist.jobgem.mapper.CommentMapper;
+import com.sist.jobgem.repository.CommentRepository;
+
+@Service
+public class CommentService {
+
+  @Autowired
+  private CommentRepository commentRepository;
+
+  // Comment 엔티티를 CommentDto로 변환하는 메소드
+  private CommentDto convertToDto(Comment comment) {
+    return CommentDto.toDto(comment);
+  }
+
+  // 댓글 리스트
+  public List<CommentDto> getCommList(Integer boIdx) {
+    List<Comment> commentList = commentRepository.findByBoIdxAndCommStatus(boIdx, 1);
+    List<CommentDto> commentDtoList = commentList.stream()
+        .map(this::convertToDto)
+        .collect(Collectors.toList());
+    return commentDtoList;
+  }
+
+  // 댓글 삭제
+  @Transactional
+  public boolean removeComment(int id) {
+    int chk = commentRepository.updateCommentStatus(id);
+    if (chk == 1)
+      return true;
+    else
+      return false;
+  }
+
+  public boolean writeComment(int boIdx, int usIdx, String content) {
+    CommentDto cDto = new CommentDto();
+    cDto.setBoIdx(boIdx);
+    cDto.setUsIdx(usIdx);
+    cDto.setCommContent(content);
+    cDto.setCommStatus(1);
+
+    Comment comment = CommentMapper.INSTANCE.dtoToEntity(cDto);
+    Comment comm = commentRepository.save(comment);
+    return comm != null;
+  }
+
+}
