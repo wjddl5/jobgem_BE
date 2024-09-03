9f0e6ccd36a229a1af75ce429bc6915bb70b9fcf
Kim YuBin <dbqks56231106@gmail.com>
2024-09-03 18:21:41 +0900

    Feat : 회원가입 @Requestbody를 받기 위해 dto 2개가 합쳐진 dto 생성

---
+++ src/main/java/com/sist/jobgem/dto/JobseekerJoinRequest.java
@@ -0,0 +1,41 @@
+package com.sist.jobgem.dto;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+public class JobseekerJoinRequest{
+
+    @Getter
+    @Setter
+    public static class JoinUser extends UserDto {
+
+        @Email
+        private String usId;
+        @NotBlank    
+        private String usPw;
+    }
+
+    @Getter
+    @Setter
+    public static class JoinJobseeker extends JobseekerDto {
+
+        @NotBlank
+        private String joName;
+        @NotBlank
+        private String joTel;
+        @NotBlank
+        private String joGender;
+    }
+
+    @Valid
+    private JoinUser user;
+
+    @Valid
+    private JoinJobseeker jobseeker;
+}
