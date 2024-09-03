def50a6f9feb4caa6c538bcdc8edffc6d4230576
Kim YuBin <dbqks56231106@gmail.com>
2024-09-03 18:31:47 +0900

    Fix : ERD에 맞게 nullable 수정

--- src/main/java/com/sist/jobgem/entity/Jobseeker.java
+++ src/main/java/com/sist/jobgem/entity/Jobseeker.java
@@ -7,28 +7,33 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 @Entity
 @Table(name = "jobseekers")
+@Builder
 public class Jobseeker {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "jo_idx", nullable = false)
     private Integer id;
 
-    @Column(name = "us_idx", nullable = false)
-    private Integer usIdx;
+    @OneToOne
+    @JoinColumn(name = "us_idx", nullable = false)
+    private User user;
 
     @Column(name = "jo_name", nullable = false, length = 10)
     private String joName;
 
-    @Column(name = "jo_birth", nullable = false)
+    @Column(name = "jo_birth")
     private LocalDate joBirth;
 
-    @Column(name = "jo_address", nullable = false, length = 30)
+    @Column(name = "jo_address", length = 30)
     private String joAddress;
 
     @Column(name = "jo_tel", nullable = false, length = 15)
@@ -40,7 +45,7 @@ public class Jobseeker {
     @Column(name = "jo_img_url", length = 100)
     private String joImgUrl;
 
-    @Column(name = "jo_edu", nullable = false, length = 10)
+    @Column(name = "jo_edu", length = 10)
     private String joEdu;
 
     @Column(name = "jo_sal", length = 10)
