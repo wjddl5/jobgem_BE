3546644d458e806cf02442e6b8da391a994f763b
jong-jang <jjh0299@gmail.com>
2024-09-03 16:11:19 +0900

    Feat : toDtoList 추가

--- src/main/java/com/sist/jobgem/mapper/JobseekerMapper.java
+++ src/main/java/com/sist/jobgem/mapper/JobseekerMapper.java
@@ -1,10 +1,14 @@
 package com.sist.jobgem.mapper;
 
+import com.sist.jobgem.dto.BoardDto;
 import com.sist.jobgem.dto.JobseekerDto;
+import com.sist.jobgem.entity.Board;
 import com.sist.jobgem.entity.Jobseeker;
 
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
+
+import java.util.List;
 
 @Mapper
 public interface JobseekerMapper {
@@ -13,4 +17,6 @@ public interface JobseekerMapper {
     JobseekerDto toDto(Jobseeker jobseeker);
 
     Jobseeker toEntity(JobseekerDto jobseekerDto);
+
+    List<JobseekerDto> toDtoList(List<Jobseeker> list);
 }
