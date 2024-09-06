83d51e40f8a89015a5b95b97fc06031f8286f5f9
henny <67935118+hykim123@users.noreply.github.com>
2024-09-06 10:02:26 +0900

    Feat: 이력서 CRUD (#45)

--- src/main/java/com/sist/jobgem/entity/Resume.java
+++ src/main/java/com/sist/jobgem/entity/Resume.java
@@ -1,15 +1,28 @@
 package com.sist.jobgem.entity;
 
+import java.time.LocalDate;
+
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.DynamicInsert;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@DynamicInsert
 @Getter
 @Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "resumes")
 public class Resume {
     @Id
@@ -26,9 +39,20 @@ public class Resume {
     @Column(name = "re_content", nullable = false, length = 100)
     private String reContent;
 
-    @Column(name = "re_fileUrl", length = 30)
-    private String reFileurl;
+    @Column(name = "re_file_url", length = 30)
+    private String reFileUrl;
 
+    @Column(name = "re_write_date", nullable = false)
+    private LocalDate reWriteDate;
+
+    @PrePersist
+    protected void onCreate() {
+        if (this.reWriteDate == null) {
+            this.reWriteDate = LocalDate.now();
+        }
+    }
+
+    @ColumnDefault("1")
     @Column(name = "re_state", nullable = false)
     private Integer reState;
 
