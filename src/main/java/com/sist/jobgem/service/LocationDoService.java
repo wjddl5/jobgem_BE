a9ffa7def09db3c43137c5273637bfb2c0460309
DESKTOP-P15R8BC\JY <wjddl5796@naver.com>
2024-09-08 17:28:14 +0900

    Feat: 지역카테고리 관리 백앤드 부분

---
+++ src/main/java/com/sist/jobgem/service/LocationDoService.java
@@ -0,0 +1,46 @@
+package com.sist.jobgem.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.sist.jobgem.dto.LocationDoDto;
+import com.sist.jobgem.entity.LocationDo;
+import com.sist.jobgem.mapper.LocationDoMapper;
+import com.sist.jobgem.repository.LocationDoRepository;
+import java.util.List;
+
+@Service
+public class LocationDoService {
+
+  @Autowired
+  private LocationDoRepository locationDoRepository;
+
+  public List<LocationDoDto> getLoc() {
+    List<LocationDo> list = locationDoRepository.findAll();
+    return LocationDoMapper.INSTANCE.toDtoList(list);
+  }
+
+  public boolean addLoc(String itemName) {
+    LocationDoDto lDto = new LocationDoDto();
+    lDto.setName(itemName);
+
+    LocationDo e = LocationDoMapper.INSTANCE.toEntity(lDto);
+
+    return locationDoRepository.save(e) != null;
+  }
+
+  public boolean removeLoc(int id) {
+    try {
+      locationDoRepository.deleteById(id);
+      return true;
+    } catch (Exception e) {
+      return false;
+    }
+  }
+
+  @Transactional
+  public boolean editLoc(int id, String editItemName) {
+    return locationDoRepository.editLoc(id, editItemName) > 0;
+  }
+}
