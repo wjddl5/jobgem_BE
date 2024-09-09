a1d15789d9b50153cc3468cf65b88d7319537da0
Kim YuBin <dbqks56231106@gmail.com>
2024-09-09 11:47:59 +0900

    Feat : 이메일 중복체크 기능 추가

--- src/main/java/com/sist/jobgem/repository/UserRepository.java
+++ src/main/java/com/sist/jobgem/repository/UserRepository.java
@@ -1,5 +1,7 @@
 package com.sist.jobgem.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,4 +11,5 @@ import com.sist.jobgem.entity.User;
 public interface UserRepository extends JpaRepository<User, Integer>{
 
     User findById(int id);
+    Optional<User> findByUsId(String usId);
 }
