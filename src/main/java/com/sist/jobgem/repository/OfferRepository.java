e8001c12e3812fbe66fa99dd4decf85bca6ea174
henny <hyeyoon4018@gmail.com>
2024-09-09 16:35:48 +0900

    Feat: 입사제안 목록 구현

--- src/main/java/com/sist/jobgem/repository/OfferRepository.java
+++ src/main/java/com/sist/jobgem/repository/OfferRepository.java
@@ -1,10 +1,13 @@
 package com.sist.jobgem.repository;
 
 import com.sist.jobgem.entity.Offer;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface OfferRepository extends JpaRepository<Offer, Integer> {
-    
+    Page<Offer> findByJoIdxAndOfState(int idx, int ofState, Pageable pageable);
 }
