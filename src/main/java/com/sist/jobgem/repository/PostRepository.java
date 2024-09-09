c920ecce4b7d9f62b570a67c027ab9146c3daf6f
leesh9 <lokciginer@gmail.com>
2024-09-09 17:13:21 +0900

    Feat: 공고 리스트 불러오기

--- src/main/java/com/sist/jobgem/repository/PostRepository.java
+++ src/main/java/com/sist/jobgem/repository/PostRepository.java
@@ -8,14 +8,16 @@ import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.querydsl.binding.QuerydslPredicate;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
 import java.util.List;
 
+
 @Repository
-public interface PostRepository extends JpaRepository<Post, Integer> {
+public interface PostRepository extends JpaRepository<Post, Integer>, PostRepositoryCustom {
     int countByCoIdxAndPoState(int coIdx, int poState);
     
     @Query("SELECT new com.sist.jobgem.dto.PostCountApplyDto(p.id, p.coIdx, p.poTitle, p.poContent, p.poDate, p.poDeadline, p.poImgurl, p.poSal, p.poWorkhour, p.poSubType, p.poAddr, p.poEmail, p.poFax, p.poState, CAST(COUNT(a) AS INTEGER) as applyCount) " +
@@ -24,8 +26,13 @@ public interface PostRepository extends JpaRepository<Post, Integer> {
             "GROUP BY p.id, p.coIdx, p.poTitle, p.poContent, p.poDate, p.poDeadline, p.poImgurl, p.poSal, p.poWorkhour, p.poSubType, p.poAddr, p.poEmail, p.poFax, p.poState")
     List<PostCountApplyDto> findAllWithApplyCount(@Param("coIdx")Integer coIdx);
 
+    int countByPoStateAndCoIdx(int poState, int coIdx);
+    int countByCoIdx(int coIdx);
+    int countByCoIdxAndPoDeadline(int coIdx, LocalDate poDeadline);
+    
     @Modifying
     @Transactional
     @Query("UPDATE Post p SET p.poState = 2 WHERE p.poDeadline <= CURRENT_DATE")
     void updateStateByDeadline();
+    
 }
---
+++ src/main/java/com/sist/jobgem/repository/PostRepositoryImpl.java
@@ -0,0 +1,64 @@
+package com.sist.jobgem.repository;
+
+
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.Projections;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.sist.jobgem.dto.PostCountApplyDto;
+import com.sist.jobgem.entity.Post;
+import com.sist.jobgem.entity.QApplyment;
+import com.sist.jobgem.entity.QPost;
+
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.QueryByExampleExecutor;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Map;
+@Repository
+@RequiredArgsConstructor
+public class PostRepositoryImpl implements PostRepositoryCustom {
+    private final JPAQueryFactory queryFactory;
+    
+
+    @Override
+    public List<PostCountApplyDto> findByFilterWithApplyCount(Map<String, Object> map) {
+        QPost post = QPost.post;
+        QApplyment applyment = QApplyment.applyment;
+        BooleanBuilder builder = new BooleanBuilder();
+        if (map.get("coIdx") != null) {
+            builder.and(post.coIdx.eq(Integer.parseInt(map.get("coIdx").toString())));
+        }
+        if (map.get("deadline") != null) {
+            builder.and(post.poDeadline.eq(LocalDate.now()));
+        }
+        if (map.get("state") != null) {
+            System.err.println("poState: " + map.get("state"));
+            builder.and(post.poState.eq(Integer.parseInt(map.get("state").toString())));
+        }
+        if (map.get("searchType") != null) {
+            if(map.get("searchType").toString().equals("title")){
+                builder.and(post.poTitle.contains(map.get("searchWord").toString()));
+            }else if(map.get("searchType").toString().equals("content")){
+                builder.and(post.poContent.contains(map.get("searchWord").toString()));
+            }
+        }
+        return queryFactory
+            .select(Projections.constructor(PostCountApplyDto.class,
+                post.id, post.coIdx, post.poTitle, post.poContent, post.poDate, post.poDeadline,
+                post.poImgurl, post.poSal, post.poWorkhour, post.poSubType, post.poAddr,
+                post.poEmail, post.poFax, post.poState,
+                applyment.count().intValue().as("applyCount")))
+            .from(post)
+            .leftJoin(applyment).on(post.id.eq(applyment.poIdx))
+            
+            .where(builder)
+            .groupBy(post.id, post.coIdx, post.poTitle, post.poContent, post.poDate, post.poDeadline,
+                post.poImgurl, post.poSal, post.poWorkhour, post.poSubType, post.poAddr,
+                post.poEmail, post.poFax, post.poState)
+            .fetch();
+    }
+}
