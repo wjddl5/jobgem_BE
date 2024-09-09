eae71fcab93844134394088541fd2902e3a7d0b0
jong-jang <jjh0299@gmail.com>
2024-09-09 16:56:08 +0900

    Fix : 머지 충돌 소스 수정

--- src/main/java/com/sist/jobgem/dto/HireKindDto.java
+++ src/main/java/com/sist/jobgem/dto/HireKindDto.java
@@ -1,12 +1,5 @@
 package com.sist.jobgem.dto;
 
-<<<<<<< HEAD
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter
-@Setter
-=======
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -16,7 +9,6 @@ import lombok.NoArgsConstructor;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
->>>>>>> 3478be65b4c004f21ff9bfcd354150278af49786
 public class HireKindDto {
     private Integer id;
     private String hkName;
