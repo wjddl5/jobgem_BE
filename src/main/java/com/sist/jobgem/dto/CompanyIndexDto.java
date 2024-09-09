15ef317a007670d444f8ab06eac0f65b66a2de23
jong-jang <jjh0299@gmail.com>
2024-09-09 16:33:58 +0900

    Fix : Page -> List

--- src/main/java/com/sist/jobgem/dto/CompanyIndexDto.java
+++ src/main/java/com/sist/jobgem/dto/CompanyIndexDto.java
@@ -3,7 +3,8 @@ package com.sist.jobgem.dto;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.data.domain.Page;
+
+import java.util.List;
 
 @Getter
 @Setter
@@ -16,9 +17,9 @@ public class CompanyIndexDto {
     private Integer interviewCount;
     private Integer talentCount;
     private Integer fitJobseekerCount;
-    private Page<BlockDto> blockList;
+    private List<BlockDto> blockList;
 
-    private CompanyIndexDto(CompanyDto company, int postCount, int noPostCount, int reviewCount, int interviewCount, int talentCount, int fitJobseekerCount, Page<BlockDto> blockList) {
+    private CompanyIndexDto(CompanyDto company, int postCount, int noPostCount, int reviewCount, int interviewCount, int talentCount, int fitJobseekerCount, List<BlockDto> blockList) {
         this.company = company;
         this.postCount = postCount;
         this.noPostCount = noPostCount;
