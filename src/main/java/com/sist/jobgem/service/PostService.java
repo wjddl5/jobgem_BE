a5d6f268b7752ef45400ab3f8c54ef9f33f329cf
leesh9 <lokciginer@gmail.com>
2024-09-09 18:27:49 +0900

    Feat: 공고 검색기능 추가

--- src/main/java/com/sist/jobgem/service/PostService.java
+++ src/main/java/com/sist/jobgem/service/PostService.java
@@ -1,9 +1,12 @@
 package com.sist.jobgem.service;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
-
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Map;
@@ -53,6 +56,7 @@ public class PostService {
 
     public PostListDto getPosts(Map<String, Object> map, int coIdx) {
         PostListDto postListDto = new PostListDto();
+        Pageable pageable = PageRequest.of(0, 10);
         List<PostCountApplyDto> postList = postRepository.findByFilterWithApplyCount(map);
         postListDto.setPostList(postList);
         setPostList(postListDto, coIdx);
