2a922e5f5df206b2aa4fcdbd665c4c1c84166b4e
geummo <kummo99@naver.com>
2024-09-11 10:52:19 +0900

    Feat: 관리자 기업 블랙리스트 추가 및 체크박스 기능 구현

--- src/main/java/com/sist/jobgem/controller/CompanyController.java
+++ src/main/java/com/sist/jobgem/controller/CompanyController.java
@@ -74,4 +74,28 @@ public class CompanyController {
     public ResponseEntity<List<ReviewDto>> getReviewListByCoIdx(int coIdx) {
         return ResponseEntity.ok(reviewService.getReviewListByCoIdx(coIdx));
     }
+
+    @GetMapping("/blackList")
+    public Page<BlockDto> getBlackList(@RequestBody Pageable pageable, @RequestParam(required = false) String value, @RequestParam(required = false) String type) {
+        return blockService.blackcompanyList(pageable, value, type);
+    }
+
+    @GetMapping("/notBlack")
+    public List<CompanyDto> notBlack(@RequestBody @RequestParam(required = false) String type, @RequestParam(required = false) String value) {
+        return companyService.notBlack(type, value);
+    }
+
+    @GetMapping("/addcompanyBlock")
+    public BlockDto addcompanyBlock(@RequestBody BlockDto dto) {
+        return blockService.addcompanyBlock(dto);
+    }
+    
+    @GetMapping("deletecompanyBlock")
+    public int deletecompanyBlock(@RequestParam List<String> chkList) {
+        for (int i = 0; i < chkList.size(); i++) {
+            blockService.deletecomjobBlock(Integer.parseInt(chkList.get(i)));
+        }
+        return chkList.size();
+    }
+
 }
