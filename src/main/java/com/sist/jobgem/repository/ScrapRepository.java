77e78b02cc874fdcbf853aea615af400c9edbafe
henny <hyeyoon4018@gmail.com>
2024-09-12 10:18:04 +0900

    Feat: 마이페이지 개수세는 함수들 구현

--- src/main/java/com/sist/jobgem/repository/ScrapRepository.java
+++ src/main/java/com/sist/jobgem/repository/ScrapRepository.java
@@ -2,9 +2,14 @@ package com.sist.jobgem.repository;
 
 import com.sist.jobgem.entity.Scrap;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ScrapRepository extends JpaRepository<Scrap, Integer> {
-    
+
+    // joIdx로 scState가 1인 Scrap 개수를 세는 쿼리 메서드
+    @Query("SELECT COUNT(s) FROM Scrap s WHERE s.joIdx = :joIdx AND s.scState = 1")
+    int countByJoIdxAndScStateIsOne(@Param("joIdx") Integer joIdx);
 }
