f95eee146077e6604b4abca5dac42f536fa06c60
jong-jang <jjh0299@gmail.com>
2024-09-18 00:11:25 +0900

    Feat : 기업 메인 채팅목록 추가

--- src/main/java/com/sist/jobgem/dto/CompanyIndexDto.java
+++ src/main/java/com/sist/jobgem/dto/CompanyIndexDto.java
@@ -1,14 +1,14 @@
 package com.sist.jobgem.dto;
 
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import java.util.List;
 
 @Getter
 @Setter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class CompanyIndexDto {
     private CompanyDto company;
     private Integer postCount;
@@ -18,17 +18,5 @@ public class CompanyIndexDto {
     private Integer talentCount;
     private Integer fitJobseekerCount;
     private List<BlockResponseDto> blockList;
-
-    private CompanyIndexDto(CompanyDto company, int postCount, int noPostCount, int reviewCount, int interviewCount, int talentCount, int fitJobseekerCount, List<BlockResponseDto> blockList) {
-        this.company = company;
-        this.postCount = postCount;
-        this.noPostCount = noPostCount;
-        this.reviewCount = reviewCount;
-        this.interviewCount = interviewCount;
-        this.talentCount = talentCount;
-        this.fitJobseekerCount = fitJobseekerCount;
-        this.blockList = blockList;
-    }
-
-    public CompanyIndexDto() {}
+    private List<ChatroomResponseDto> chatList;
 }
