417ce0e6e9485b38f4cd721bbc6db55ab5b02c95
meyoong <yoongo_o@naver.com>
2024-09-18 18:36:35 +0900

    Feat: 파일처리 구현중

---
+++ src/main/java/com/sist/jobgem/service/S3UploadService.java
@@ -0,0 +1,60 @@
+package com.sist.jobgem.service;
+
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class S3UploadService {
+
+    private final AmazonS3 amazonS3;
+
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucket;
+
+    // 파일 업로드
+    // 중복이름 처리 uuid 검색해보기
+    public String saveFile(MultipartFile multipartFile) throws IOException {
+        String originalFilename = multipartFile.getOriginalFilename();
+
+        ObjectMetadata metadata = new ObjectMetadata();
+        metadata.setContentLength(multipartFile.getSize());
+        metadata.setContentType(multipartFile.getContentType());
+
+        amazonS3.putObject(bucket, originalFilename, multipartFile.getInputStream(), metadata);
+        return amazonS3.getUrl(bucket, originalFilename).toString();
+    }
+
+    // 파일 다운로드
+    public ResponseEntity<UrlResource> downloadImage(String originalFilename) {
+        UrlResource urlResource = new UrlResource(amazonS3.getUrl(bucket, originalFilename));
+
+        String contentDisposition = "attachment; filename=\"" + originalFilename + "\"";
+
+        // header에 CONTENT_DISPOSITION 설정을 통해 클릭 시 다운로드 진행
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, contentDisposition)
+                .body(urlResource);
+
+    }
+
+    // 이미지 미리보기
+    // String url = amazonS3.getUrl(bucket, filename).toString();
+    // <img th:src="${s3ImageUrl}"/>
+
+    // 파일 삭제
+    public void deleteImage(String originalFilename) {
+        amazonS3.deleteObject(bucket, originalFilename);
+    }
+}
