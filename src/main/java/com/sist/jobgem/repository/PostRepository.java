6fd05d911bc7254ed78985c61b0f69bc887322d0
geummo <kummo99@naver.com>
2024-09-19 17:49:23 +0900

    Fix: 관리자 공고 전체조회 검색 기능 수정

--- src/main/java/com/sist/jobgem/repository/PostRepository.java
+++ src/main/java/com/sist/jobgem/repository/PostRepository.java
@@ -20,59 +20,58 @@ import jakarta.transaction.Transactional;
 
 @Repository
 public interface PostRepository extends JpaRepository<Post, Integer>, PostRepositoryCustom {
-    @Query("SELECT new com.sist.jobgem.dto.PostDto(t) FROM Post t WHERE t.poState =  1")
-    Slice<PostDto> getPostListSlice(Pageable pageable);
+        @Query("SELECT new com.sist.jobgem.dto.PostDto(t) FROM Post t WHERE t.poState =  1")
+        Slice<PostDto> getPostListSlice(Pageable pageable);
 
-    int countByCoIdxAndPoState(int coIdx, int poState);
+        int countByCoIdxAndPoState(int coIdx, int poState);
 
-    @Query("SELECT new com.sist.jobgem.dto.PostCountApplyDto(p.id, p.coIdx, p.poTitle, p.poContent, p.poDate, p.poDeadline, p.poImgurl, p.poSal, p.poSubType, p.poAddr, p.poEmail, p.poFax, p.poState, CAST(COUNT(a) AS INTEGER) as applyCount) "
-            +
-            "FROM Post p LEFT JOIN Applyment a ON p.id = a.poIdx " +
-            "WHERE p.coIdx = :coIdx " +
-            "GROUP BY p.id, p.coIdx, p.poTitle, p.poContent, p.poDate, p.poDeadline, p.poImgurl, p.poSal, p.poSubType, p.poAddr, p.poEmail, p.poFax, p.poState")
-    List<PostCountApplyDto> findAllWithApplyCount(@Param("coIdx") int coIdx);
+        @Query("SELECT new com.sist.jobgem.dto.PostCountApplyDto(p.id, p.coIdx, p.poTitle, p.poContent, p.poDate, p.poDeadline, p.poImgurl, p.poSal, p.poSubType, p.poAddr, p.poEmail, p.poFax, p.poState, CAST(COUNT(a) AS INTEGER) as applyCount) "
+                        +
+                        "FROM Post p LEFT JOIN Applyment a ON p.id = a.poIdx " +
+                        "WHERE p.coIdx = :coIdx " +
+                        "GROUP BY p.id, p.coIdx, p.poTitle, p.poContent, p.poDate, p.poDeadline, p.poImgurl, p.poSal, p.poSubType, p.poAddr, p.poEmail, p.poFax, p.poState")
+        List<PostCountApplyDto> findAllWithApplyCount(@Param("coIdx") int coIdx);
 
-    int countByPoStateAndCoIdx(int poState, int coIdx);
+        int countByPoStateAndCoIdx(int poState, int coIdx);
 
-    int countByCoIdx(int coIdx);
+        int countByCoIdx(int coIdx);
 
-    int countByCoIdxAndPoDeadline(int coIdx, LocalDate poDeadline);
+        int countByCoIdxAndPoDeadline(int coIdx, LocalDate poDeadline);
 
-    @Modifying
-    @Transactional
-    @Query("UPDATE Post p SET p.poState = 2 WHERE p.poDeadline <= CURRENT_DATE")
-    void updateStateByDeadline();
+        @Modifying
+        @Transactional
+        @Query("UPDATE Post p SET p.poState = 2 WHERE p.poDeadline <= CURRENT_DATE")
+        void updateStateByDeadline();
 
-    @Query("SELECT p FROM Post p WHERE p.id = :id AND p.coIdx != 0")
-    Post findById(@Param("id") int id);
+        @Query("SELECT p FROM Post p WHERE p.id = :id AND p.coIdx != 0")
+        Post findById(@Param("id") int id);
 
-    @Query("SELECT p FROM Post p LEFT JOIN FETCH p.company WHERE p.id = :id")
-    PostDto findByIdWithCompany(@Param("id") int id);
+        @Query("SELECT p FROM Post p LEFT JOIN FETCH p.company WHERE p.id = :id")
+        PostDto findByIdWithCompany(@Param("id") int id);
 
-    @Query("SELECT p.poTitle FROM Post p WHERE p.id = :id")
-    String findTitleById(@Param("id") int id);
+        @Query("SELECT p.poTitle FROM Post p WHERE p.id = :id")
+        String findTitleById(@Param("id") int id);
 
-    @Modifying
-    @Transactional
-    @Query("UPDATE Post p SET p.poState = 0 WHERE p.id = :id")
-    int updateStateById(@Param("id") int id); 
+        @Modifying
+        @Transactional
+        @Query("UPDATE Post p SET p.poState = 0 WHERE p.id = :id")
+        int updateStateById(@Param("id") int id);
 
-    Page<Post> findByPoTitleContainsAndPoState(String keyword, int poState, Pageable pageable);
-    
-    @Query("SELECT p FROM Post p JOIN Company c ON p.coIdx = c.id " +
-            "WHERE p.poState = 1 AND (" +
-            "(:type IS NULL AND :value IS NULL) OR " +
-            "(:type = 'title' AND p.poTitle LIKE CONCAT('%', :value, '%')) OR " +
-            "(:type = 'date' AND CAST(p.poDate AS string) LIKE CONCAT('%', :value, '%')) OR " +
-            "(:type = 'deadline' AND CAST(p.poDeadline AS string) LIKE CONCAT('%', :value, '%')) OR " +
-            "(:type = 'sal' AND p.poSal LIKE CONCAT('%', :value, '%')) OR " +
-            "(:type = 'startTime' AND p.wsStartTime LIKE CONCAT('%', :value, '%')) OR " +
-            "(:type = 'endTime' AND p.wsEndTime LIKE CONCAT('%', :value, '%')) OR " +
-            "(:type = 'type' AND p.poSubType LIKE CONCAT('%', :value, '%')) OR " +
-            "(:type = 'addr' AND p.poAddr LIKE CONCAT('%', :value, '%')) OR " +
-            "(:type = 'email' AND p.poEmail LIKE CONCAT('%', :value, '%')) OR " +
-            "(:type = 'fax' AND p.poFax LIKE CONCAT('%', :value, '%'))" +
-            ")")
-    Page<Post> findByTitleOrContent(@Param("value") String value, @Param("type") String type, Pageable pageable);
+        Page<Post> findByPoTitleContainsAndPoState(String keyword, int poState, Pageable pageable);
 
+        @Query("SELECT p FROM Post p JOIN Company c ON p.coIdx = c.id " +
+                        "WHERE (" +
+                        "(:type IS NULL AND :value IS NULL) OR " +
+                        "(:type = 'title' AND p.poTitle LIKE CONCAT('%', :value, '%')) OR " +
+                        "(:type = 'date' AND CAST(p.poDate AS string) LIKE CONCAT('%', :value, '%')) OR " +
+                        "(:type = 'deadline' AND CAST(p.poDeadline AS string) LIKE CONCAT('%', :value, '%')) OR " +
+                        "(:type = 'sal' AND p.poSal LIKE CONCAT('%', :value, '%')) OR " +
+                        "(:type = 'startTime' AND p.wsStartTime LIKE CONCAT('%', :value, '%')) OR " +
+                        "(:type = 'endTime' AND p.wsEndTime LIKE CONCAT('%', :value, '%')) OR " +
+                        "(:type = 'type' AND p.poSubType LIKE CONCAT('%', :value, '%')) OR " +
+                        "(:type = 'addr' AND p.poAddr LIKE CONCAT('%', :value, '%')) OR " +
+                        "(:type = 'email' AND p.poEmail LIKE CONCAT('%', :value, '%')) OR " +
+                        "(:type = 'fax' AND p.poFax LIKE CONCAT('%', :value, '%'))" +
+                        ")")
+        Page<Post> findByTitleOrContent(@Param("value") String value, @Param("type") String type, Pageable pageable);
 }
