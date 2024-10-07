0c57cf03ca6086aa44dd9cd224f6dea4fe1cceb5
Kim YuBin <dbqks56231106@gmail.com>
2024-10-07 15:49:06 +0900

    Fix : 빌드 오류로 테스트 코드 삭제

--- src/test/java/com/sist/jobgem/JobgemApplicationTests.java
+++ src/test/java/com/sist/jobgem/JobgemApplicationTests.java
@@ -1,13 +1,5 @@
 package com.sist.jobgem;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
 class JobgemApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
