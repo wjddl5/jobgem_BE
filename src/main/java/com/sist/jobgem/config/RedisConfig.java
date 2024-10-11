5fbd816ea32184ce6d2da19a5ab7fd60a60a4671
Kim YuBin <dbqks56231106@gmail.com>
2024-10-11 15:50:10 +0900

    Feat : Redis 도커 컨테이너로 연결

--- src/main/java/com/sist/jobgem/config/RedisConfig.java
+++ src/main/java/com/sist/jobgem/config/RedisConfig.java
@@ -3,6 +3,7 @@ package com.sist.jobgem.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
@@ -14,7 +15,7 @@ public class RedisConfig {
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
         // application.yml에 설정된 Redis 서버 주소와 포트로 연결
-        return new LettuceConnectionFactory();
+        return new LettuceConnectionFactory(new RedisStandaloneConfiguration("redis", 6379));
     }
 
     @Bean
